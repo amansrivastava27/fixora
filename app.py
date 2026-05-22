3964a5a9b4a96ed523e53d2b75c86f26d18fdf43
amansrivastava27 <srivasaman786@gmail.com>
2026-05-22 16:44:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fixora.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
